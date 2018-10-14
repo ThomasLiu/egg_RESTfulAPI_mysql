014c9ead9d2f6cd0420adc8bdc41fc27e3d0f1dc
Thomas Lau <thomas_0836@qq.com>
2018-10-14 16:48:16 +0800

    fix

--- config/config.local.js
+++ config/config.local.js
@@ -7,9 +7,9 @@ module.exports = () => {
   config.sequelize = {
     dialect: 'mysql',
     port: 3306,
-    hostname: '127.0.0.1',
+    // host: '127.0.0.1', 如果也是用127.0.0.1就不用改
     database: 'egg_RESTfulAPI_mysql_development',
-    user: 'dbUserName',
+    // username: 'dbUserName', 如果也是用root就不用改
     password: '123456789',
     define: {
       underscored: false,
--- config/config.prod.js
+++ config/config.prod.js
@@ -7,10 +7,8 @@ module.exports = () => {
   config.sequelize = {
     dialect: 'mysql',
     port: 3306,
-    hostname: '123123123141231.mysql.rds.aliyuncs.com',
     host: '123123123141231.mysql.rds.aliyuncs.com',
     database: 'egg_RESTfulAPI_mysql',
-    user: 'dbUserName',
     username: 'dbUserName',
     password: '123456789',
     define: {
